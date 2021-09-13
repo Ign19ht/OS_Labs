6498587917d1f1087425ff326cf8cf6424456071
ignat <petrov-ignat85@mail.ru>
2021-09-13 17:38:46 +0300

    updated week04/ex4.c

--- week04/ex4.c
+++ week04/ex4.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(void) {
-    while (1) {
-        char input[15];
-        scanf("%s", &input);
-        system(input);
+    char *input = malloc(sizeof(char));
+    scanf("%[^\n]s", input);
+    printf("%s", input);
+    char *command = malloc(sizeof(char));
+    int i = 0;
+    while (*input && *input != ' ') {
+        *(command + i) = *input;
+        i++;
+        input++;
+    }
+    char *args[8];
+    args[0] = command;
+    int arg_counter = 0;
+    i = 0;
+    while (*input) {
+        if (*input == ' ') {
+            arg_counter++;
+            args[arg_counter] = malloc(sizeof(char));
+            i = 0;
+        } else {
+            *(args[arg_counter] + i) = *input;
+            i++;
+        }
+        input++;
+    }
+    if (arg_counter == 0) system(command);
+    else {
+        args[arg_counter + 1] = NULL;
+        char *environ[] = {NULL};
+        execve(command, args, environ);
     }
 }
