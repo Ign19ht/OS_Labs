e150361e21bdb412ef4bd9d057441f262c97bd4a
ignat <petrov-ignat85@mail.ru>
2021-09-14 22:39:18 +0300

    finished week04/ex4.c

--- week04/ex4.c
+++ week04/ex4.c
@@ -3,35 +3,10 @@
 #include <unistd.h>
 
 int main(void) {
-    char *input = malloc(sizeof(char));
-    scanf("%[^\n]s", input);
-    printf("%s", input);
-    char *command = malloc(sizeof(char));
-    int i = 0;
-    while (*input && *input != ' ') {
-        *(command + i) = *input;
-        i++;
-        input++;
-    }
-    char *args[8];
-    args[0] = command;
-    int arg_counter = 0;
-    i = 0;
-    while (*input) {
-        if (*input == ' ') {
-            arg_counter++;
-            args[arg_counter] = malloc(sizeof(char));
-            i = 0;
-        } else {
-            *(args[arg_counter] + i) = *input;
-            i++;
-        }
-        input++;
-    }
-    if (arg_counter == 0) system(command);
-    else {
-        args[arg_counter + 1] = NULL;
-        char *environ[] = {NULL};
-        execve(command, args, environ);
+    while (1) {
+        char *input = malloc(sizeof(char));
+        size_t bufsize = 256;
+        getline(&input, &bufsize, stdin);
+        system(input);
     }
 }
